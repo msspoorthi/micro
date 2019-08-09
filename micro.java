094b79c514381a60bf28fd0512a1e431afd79cd5
msspoorthi <53563140+msspoorthi@users.noreply.github.com>
2019-08-09 17:02:28 +0530

    Add files via upload

---
+++ micro.java
@@ -0,0 +1,30 @@
+package microaarray;
+import java.util.*;
+public class mc 
+{
+
+   public static void main(String args[] ) throws Exception {
+
+     Scanner sc = new Scanner(System.in);
+
+     int t = sc.nextInt();
+
+
+
+       for (int i = 0; i < t; i++) {
+
+          int n= sc.nextInt();
+
+          int k = sc.nextInt();
+
+          int arr[] = new int[n];
+
+          
+
+            for(int j=0; j<n;j++){
+
+             arr[j]=sc.nextInt();
+
+              
+
+            }
